a020db5009b02bf106fc51c4a93db1b116fb93fc
Jordi Chulia <jorchube.dev@gmail.com>
2023-02-25 16:40:14 +0000

    Refactor parsing updates

--- src/cacabot.py
+++ src/cacabot.py
@@ -18,25 +18,28 @@ class Cacabot:
     def _get_cacas_from_updates(self, updates) -> list[Caca]:
         cacas = list()
         for update in updates:
-
             if not self._is_a_text_message(update):
                 continue
-
-            update_text = update["message"]["text"]
-
-            if update_text != "💩":
+            if not self._is_valid_caca(update):
                 continue
 
-            caca = Caca(
-                update_offset=update["update_id"],
-                chat_id=str(update["message"]["chat"]["id"]),
-                chat_name=str(update["message"]["chat"]["title"]),
-                chat_member_name=str(update["message"]["from"]["first_name"]),
-                chat_member_id=str(update["message"]["from"]["id"]),
-            )
+            caca = self._create_caca_from_update(update)
             cacas.append(caca)
 
         return cacas
 
+    def _create_caca_from_update(self, update):
+        return Caca(
+            update_offset=update["update_id"],
+            chat_id=str(update["message"]["chat"]["id"]),
+            chat_name=str(update["message"]["chat"]["title"]),
+            chat_member_name=str(update["message"]["from"]["first_name"]),
+            chat_member_id=str(update["message"]["from"]["id"]),
+        )
+
     def _is_a_text_message(self, update):
         return "message" in update and "text" in update["message"]
+
+    def _is_valid_caca(self, update):
+        update_text = update["message"]["text"]
+        return update_text == "💩"
